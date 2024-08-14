946725bb2c83356fa3bb61a14674654decfb3a63
Iury Santos <iusantos@c1r7p4.42sp.org.br>
2024-08-14 11:02:15 -0300

    ex00: final touch

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -3,11 +3,19 @@
 
 int main(void)
 {
+	std::cout << "Fixed    a;" << std:: endl;
 	Fixed	a;
-	Fixed	b(a);
+	std::cout << std::endl;
 
+	std::cout << "Fixed    b(a);" << std::endl;
+	Fixed	b(a);
+	std::cout << std::endl;
+
+	std::cout << "Fixed    c;" << std::endl;
+	std::cout << "c = b;" << std::endl;
 	Fixed	c;
 	c = b;
+	std::cout << std::endl;
 
 	std::cout << a.getRawBits() << std::endl;
 	std::cout << b.getRawBits() << std::endl;
